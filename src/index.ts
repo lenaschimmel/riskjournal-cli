256b4fb5685062b1d753bd10e49727bc2aaf7cab
Lena Schimmel <mail@lenaschimmel.de>
2020-11-15 18:14:46 +0100

    Better error- and exit handling.

--- src/index.ts
+++ src/index.ts
@@ -7,33 +7,42 @@ import https from 'https';
 import http from 'http';
 var targz = require('targz');
 import { BASE_URL} from './constants';
+require('trace-unhandled/register');
 
 var profile: Profile | null = null;
 
-async function main() {
-  console.log("Lade aktuelle Inzidenz-Werte herunter…");
-  await downloadIncidence();
-  console.log("…Inzidenz-Werte fertig.");
-  let timer = setInterval(downloadIncidence, 3600 * 1000); // 1 hour
+process.on('warning', e => console.warn(e.stack));
 
-  let response = await prompt({
-    type: 'select',
-    name: 'username',
-    message: 'Bitte Profilnamen auswählen:',
-    choices: [... Profile.getProfileChoices(), {name:"<new>", message: "<Neu anlegen>"}],
-  });
-  
-  if (response.username == "<new>") {
-    response = await prompt({
-      type: 'input',
+async function main() {
+  try {
+    console.log("Lade aktuelle Inzidenz-Werte herunter…");
+    await downloadIncidence();
+    console.log("…Inzidenz-Werte fertig.");
+    let timer = setInterval(downloadIncidence, 3600 * 1000); // 1 hour
+
+    let response = await prompt({
+      type: 'select',
       name: 'username',
-      message: 'Bitte neuen Profilnamen eingeben:',
+      message: 'Bitte Profilnamen auswählen:',
+      choices: [... Profile.getProfileChoices(), {name:"<new>", message: "<Neu anlegen>"}],
     });
-    fs.mkdirSync("./data/" + response.username);
+    
+    if (response.username == "<new>") {
+      response = await prompt({
+        type: 'input',
+        name: 'username',
+        message: 'Bitte neuen Profilnamen eingeben:',
+      });
+      fs.mkdirSync("./data/" + response.username);
+    }
+    profile = new Profile(response.username);
+    await profile.run();
+    clearInterval(timer);
+  } catch(e) {
+    // we need this, or pressing crtl-c will push the node process into limbo.
+    console.log("Cought top-level exception. Bye!");
+    process.exit(1);
   }
-  profile = new Profile(response.username);
-  await profile.run();
-  clearInterval(timer);
 }
 
 async function downloadIncidence() {
